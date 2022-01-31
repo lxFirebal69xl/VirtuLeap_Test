a79c750c198db7d36b409d679ee529ff05130595
Ruben Pereira <rubenpereira.info@gmail.com>
2022-01-31 16:33:59 +0000

    -Finalized player character by removing excess mechanics. -Created first level design -Integrated post processing -Made overall scene more attractive, adding fx, lighting and small details -Audio group set up, with a master, sfx, voice, ambiance and music sliders -Added fog to the level to complement the look -Fixed an issue causing the projectiles not line up properly with the camera

---
+++ Assets/Misc_Info.cs
@@ -0,0 +1,10 @@
+﻿
+
+
+//Assets used
+
+//https://assetstore.unity.com/packages/3d/vegetation/flowers/hanging-plants-free-173472
+//https://assetstore.unity.com/packages/3d/environments/dungeons/decrepit-dungeon-lite-33936
+//https://assetstore.unity.com/packages/vfx/particles/fire-explosions/procedural-fire-141496
+//https://assetstore.unity.com/packages/tools/input-management/simple-fps-controller-v1-4-173881
+//https://assetstore.unity.com/packages/vfx/shaders/ultra-skybox-fog-115414
---
+++ Assets/Scripts/Projectile_Explosion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile_Explosion : MonoBehaviour
+{
+
+    public GameObject explosionVFX;
+
+
+
+    void OnCollisionEnter(Collision other)
+    {
+
+        Debug.Log(other);
+        Destroy(gameObject);
+        Instantiate(explosionVFX, transform.position, transform.rotation);
+
+
+    }
+
+
+}
